4e30598e7ae71af6bbb1007372477be966ac5db6
“TobiasBarbosa” <toha0006@stud.kea.dk>
2024-09-19 08:51:33 +0200

    Lavet flere enum tags og tilføjet constructor og displayName

--- src/main/java/com/example/touristguide/model/Tag.java
+++ src/main/java/com/example/touristguide/model/Tag.java
@@ -1,7 +1,37 @@
 package com.example.touristguide.model;
 
 public enum Tag {
-    STATUE, SEVÆRDIGHED, FORLYSTELSESPARK, UNDERHOLDNING, RESTAURANT, KONCERT;
 
-    //TODO lav andre tags
+    //***ENUM***--------------------------------------------------------------------------------------------------------
+    //TODO lav andre tags ??
+    STATUE          ("Statue"),
+    SEVÆRDIGHED     ("Seværdighed"),
+    FORLYSTELSESPARK("Forlystelsespark"),
+    UNDERHOLDNING   ("Underholdning"),
+    RESTAURANT      ("Restaurant"),
+    KONCERT         ("Koncert"),
+    TEATER          ("Teater"),
+    NATUR           ("Natur"),
+    MUSEUM          ("Museum"),
+    SPORT           ("Sport"),
+    PARK            ("Park"),
+    DESIGN          ("Design"),
+    ARKITEKTUR      ("Arkitektur"),
+    MONUMENTER      ("Monumenter");
+
+    //***ATTRIBUTES***--------------------------------------------------------------------------------------------------
+    private final String displayName;
+
+    //***CONSTRUCTOR***-------------------------------------------------------------------------------------------------
+    //Enum constructor må ikke være public af en eller anden årsag...
+    Tag(String displayName){
+        this.displayName = displayName;
+    }
+
+    //***GETTER METHOD***-----------------------------------------------------------------------------------------------
+    public String getDisplayName(){
+        return displayName;
+    }
+
+    //***END***---------------------------------------------------------------------------------------------------------
 }
